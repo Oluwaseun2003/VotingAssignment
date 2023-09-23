384222e7a14bd46e3b67084102e26af550c4fe91
Oluwaseun2003 <oluwafemioluwaseun421@gmail.com>
2023-09-23 23:03:09 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+//some conditon commands
+//< = less than
+// > = greater than
+// <= = grater than or equals to
+// >= = greater than or equals to
+// == = double equals to(equals to)
+// === = triple equals to(strict equals to)(does not only check on values, it checks data types also)
+// ! = not equals to
+
+// || = or
+// && = and
+//  an else if 
+
+
+
+// let age = 18;
+
+// if (age > 18) {
+//     console.log("yes, It is greater")
+// } else if (age >= 20){
+//     console.log("yes age")
+// } else {
+//     console.log("No condition")
+// }
+
+
+let dogBark  =  true;
+let lionRoar = false
+
+if (!dogBark) {
+console.log("age is greater than 20")
+} else if (!lionRoar) {
+console.log("age is less than 3")
+}
+else if (!!dogBark) {
+console.log("maybe") 
+}
+else {
+console.log("no")
+}
+ 
